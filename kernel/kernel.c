182d47d8db82e805668f6fb9e6baeb0c94af274a
SirBrahms <95311570+SirBrahms@users.noreply.github.com>
2025-02-19 11:52:20 +0100

    Add linker file and added empty kernel_main

---
+++ kernel/kernel.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "include/tty.h"
+
+
+// Compile Time checks
+#if defined(__linux__)
+#error "Not using a cross compiler"
+#endif
+
+#if !defined(__i386__)
+#error "Not compiling to i386"
+#endif
+
+void kernel_main(void) {
+
+	return;
+}
