ec01e28c85fbe619c74def34fd5a33c857a8bf43
SirBrahms <95311570+SirBrahms@users.noreply.github.com>
2025-02-23 12:57:12 +0100

    Add more libc header files

---
+++ libc/include/stdlib.h
@@ -0,0 +1,15 @@
+#ifndef __STDLIB_HEADER
+#define __STDLIB_HEADER
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+__attribute__((__noreturn__))
+void abort(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ libc/include/sys/cdefs.h
@@ -0,0 +1,6 @@
+#ifndef __SYS_CDEFS_HEADER
+#define __SYS_CDEFS_HEADER
+
+#define __tunix_libc 1
+
+#endif
