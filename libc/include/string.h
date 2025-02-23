10832f4fed8a0e76569574b9944a23a805c8daa3
SirBrahms <95311570+SirBrahms@users.noreply.github.com>
2025-02-23 12:47:18 +0100

    Beginnings of string.h implementation

---
+++ libc/include/string.h
@@ -0,0 +1,16 @@
+#ifndef __STRING_HEADER
+#define __STRING_HEADER
+
+#include <stddefs.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+size_t strlen(const char*);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
