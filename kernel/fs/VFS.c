07d00f5d53cff8ce89a79f84031c2a5d1989cfee
SirBrahms <95311570+SirBrahms@users.noreply.github.com>
2025-02-28 12:51:07 +0100

    Filedescriptor implementation

--- kernel/fs/VFS.c
+++ kernel/fs/VFS.c
@@ -1,3 +1,45 @@
 #include "VFS.h"
+#include <stdbool.h>
 
 
+FS_node root = { VFS_flags.D, {}, "/" };
+fd fds[10] = { 0 };
+
+FS_node* traverse_nodes(const char* path) {
+	FS_node* current = &root;
+	while (1) {
+		if (strcmp(path, current->name))
+			break;
+	
+			
+	}
+	return current;
+}
+
+int open(const char* path, unsigned char flags) {
+	// Look for next free file descriptor
+	int nextfd = 0;
+	while (!fds[nextfd]) {
+		nextfd++;
+	}
+
+	// find FS_node at path
+	FS_node* node = traverse_nodes(path);
+	
+	// make an entry in the filedescriptor array
+	//fds[nextfd] = (fd) {flags, };
+	
+	return nextfd;
+}
+
+size_t write(int fd, char* buf) {
+
+}
+
+size_t read(int fd, void* buf, size_t count) {
+
+}
+
+int close(int fd) {
+
+}
--- kernel/fs/VFS.h
+++ kernel/fs/VFS.h
@@ -9,10 +9,30 @@ typedef struct {
 	char* name;
 } FS_node;
 
-FS_node traverse_nodes(char*);
+typedef struct {
+	int mode;
+	struct FS_node*;
+} fd;
 
-int open(const char*, const char*);
-int write(FS_node, char*);
-int read(FS_node);
-int close(FS_node);
+enum VFS_flags {
+	D  = 1,
+	F  = 2,
+	X  = 4,
+	IO = 8
+}
+
+enum file_flags {
+	RDONLY = 0b00000001,
+	WRONLY = 0b00000010,
+	APPEND = 0b00000100,
+	CREATE = 0b00001000,
+	TRUNC  = 0b00010000,
+};
+
+FS_node* traverse_nodes(const char*);
+
+int open(const char*, unsigned char);
+size_t write(int, char*);
+size_t read(int, void*, size_t);
+int close(int);
 #endif
