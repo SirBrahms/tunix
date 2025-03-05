3424dd742ae21e99f27cc68544049e606cd9f7af
SirBrahms <95311570+SirBrahms@users.noreply.github.com>
2025-03-05 13:37:20 +0100

    Start of GDT-Implementation

--- kernel/GDT.c
+++ kernel/GDT.c
@@ -1,26 +1,42 @@
 #include "include/GDT.h"
+#include "include/TSS.h"
 
-GDT GDT_table[4];
+GDT GDT_table[4] = { 0 };
 
 void gdt_init() {
-	table[0] = 0; // Null descriptor
+	// GDT_table[0] -> Null descriptor
 	
 	/* Kernel code segment */
-	table[1].limit_low = 0;
-	table[1].base_low = 0x00400000;
-	table[1].accessed = 1;
-	table[1].read_write = 0;
-	table[1].dc = 0;
-	table[1].executable = 1;
-	table[1].descriptor = 1;
-	table[1].dpl = 0;
-	table[1].present = 1;
-	table[1].limit_high = 0;
-	table[1].long_mode = 0;
-	table[1].db = 1;
-	table[1].granular = 1;
-	table[1].base_high = 0;
+	GDT_table[1].limit_low = 0;
+	GDT_table[1].base_low = 0x00400000;
+	GDT_table[1].accessed = 1;
+	GDT_table[1].read_write = 0;
+	GDT_table[1].dc = 0;
+	GDT_table[1].executable = 1;
+	GDT_table[1].descriptor = 1;
+	GDT_table[1].dpl = 0;
+	GDT_table[1].present = 1;
+	GDT_table[1].limit_high = 0;
+	GDT_table[1].long_mode = 0;
+	GDT_table[1].db = 1;
+	GDT_table[1].granular = 1;
+	GDT_table[1].base_high = 0;
 
 	/* Kernel data segment */
-	table[0].limit_low = 0; // XXX: Temp
+	GDT_table[2].limit_low = 0b1111111111111111;
+	GDT_table[2].base_low = 0x00800000;
+	GDT_table[2].accessed = 1;
+	GDT_table[2].read_write = 0;
+	GDT_table[2].dc = 0;
+	GDT_table[2].executable = 0;
+	GDT_table[2].descriptor = 1;
+	GDT_table[2].dpl = 0;
+	GDT_table[2].present = 1;
+	GDT_table[2].limit_high = 0b1111;
+	GDT_table[2].long_mode = 0;
+	GDT_table[2].db = 1;
+	GDT_table[2].granular = 1;
+	GDT_table[2].base_high = 0;
+
+	/* Task State Segment */
 }
--- kernel/include/GDT.h
+++ kernel/include/GDT.h
@@ -1,21 +1,30 @@
 #ifndef __GDT_HEADER__
 #define __GDT_HEADER__
 
+#include <stdint.h>
+
 struct _GDT {
 	unsigned int limit_low	: 16;
 	unsigned int base_low 	: 24;
 	/* Access byte */
-	unsigned int accessed	: 1;
-	unsigned int read_write	: 1;
-	unsigned int dc		: 1;
-	unsigned int executable : 1;
+	union {
+		struct { // Access byte for hardware segments
+			unsigned int accessed	: 1;
+			unsigned int read_write	: 1;
+			unsigned int dc		: 1;
+			unsigned int executable : 1;
+
+		};
+		
+		unsigned int type	: 4; // Software segments
+	};
 	unsigned int descriptor : 1;
 	unsigned int dpl	: 2;
 	unsigned int present	: 1;
 	/* End Access byte */
 	unsigned int limit_high : 4;
 	/* Flags */
-	unsigned int reserved	: 1;
+	//unsigned int _reserved	: 1;
 	unsigned int long_mode	: 1;
 	unsigned int db		: 1;
 	unsigned int granular	: 1;
