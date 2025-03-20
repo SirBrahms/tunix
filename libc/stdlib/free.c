3f719c9c1416b62cea33a2d1c283f6767d8687dd
SirBrahms <95311570+SirBrahms@users.noreply.github.com>
2025-03-20 21:57:46 +0100

    Functions no longer use old heap

--- libc/stdlib/free.c
+++ libc/stdlib/free.c
@@ -1,11 +1,9 @@
 #include <stdlib.h>
 #ifdef __is_libk
-#include "memmgr.h"
 #endif
 
 void free(void* ptr) {
 	#ifdef __is_libk
-	heap_free(__kernel_heap, ptr);
 	#else
 	#endif
 } 
--- libc/stdlib/malloc.c
+++ libc/stdlib/malloc.c
@@ -1,23 +1,9 @@
 #include <stdlib.h>
 #if defined(__is_libk)
-#include "memmgr.h"
 #endif
 
 void* malloc(size_t size) {
 #ifdef __is_libk
-	void* ret = heap_alloc(__kernel_heap, size);
-	if (ret != NULL)
-		return ret;
-
-	add_block(__kernel_heap, 0x00800000, 0x10000, 16);
-
-
-	ret = heap_alloc(__kernel_heap, size);
-	
-	if (ret == NULL)
-		abort();
-
-	return ret;
 #else
 
 #endif
