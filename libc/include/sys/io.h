8869ab8586b17afd011d2aa89e25ebf366acda8e
SirBrahms <95311570+SirBrahms@users.noreply.github.com>
2025-03-22 16:55:20 +0100

    Add outl and inl functions

--- libc/include/sys/io.h
+++ libc/include/sys/io.h
@@ -15,12 +15,23 @@ static inline unsigned short int inw(unsigned short int port) {
 	return _v;
 }
 
+static inline unsigned int inl(unsigned short int port) {
+	unsigned int _v;
+	asm volatile ("inl %w1,%0":"=a" (_v):"Nd" (port));
+
+	return _v;
+}
+
 static inline void outb(unsigned char value, unsigned short int port) {
 	asm volatile ("outb %b0,%w1": :"a" (value), "Nd" (port));
 }
 
 static inline void outw(unsigned short int value, unsigned short int port) {
 	asm volatile ("outw %w0,%w1": :"a" (value), "Nd" (port));
+}
+
+static inline void outl(unsigned int value, unsigned short int port) {
+	asm volatile ("outl %0,%w1": :"a" (value), "Nd" (port));
 }
 
 #endif
